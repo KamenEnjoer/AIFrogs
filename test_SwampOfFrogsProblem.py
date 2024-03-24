15060a564043afeb6c013cdd3e6998a937649acb
Nadezda Pyzevskaja <nadezda.pyzevskaja@stud.vilniustech.lt>
2024-03-24 17:22:37 +0200

    Frogs

--- test_SwampOfFrogsProblem.py
+++ test_SwampOfFrogsProblem.py
@@ -1,77 +1,77 @@
-#2024-03-10 Nadezda Pyzevskaja
+#2024-03-10 Nadežda Pyževskaja
 '''
-Yra nxn dydzio pelke su r varliu kiekiu.
-Reikia padeti varles taip, kad tarp ju butu kuo didesnis atstumas.
+Yra nxn dydžio pelkė su r varlių kiekių.
+Reikia padėti varles taip, kad tarp jų butu kuo didesnis atstumas.
 '''
 
 from collections import deque
 
 class Node:
-    def __init__(self, state): #Inicializacija kintamuju
+    def __init__(self, state): #Inicializacija kintamujų
         self.state = state
 
-    def expand(self, problem): #Grazina visu prioretizuotu vietu pelkeje sarasa
+    def expand(self, problem): #Gražina visu prioretizuotu vietu pelkėje sąrašą
         return [Node(problem.result(self.state, action)) for action in problem.actions(self.state)]
 
-def breadth_first_graph_search(problem): #Paieskos algoritmas
+def breadth_first_graph_search(problem): #Paieškos algoritmas
     node = Node(problem.initial)
-    if problem.goal_test(node.state): #Ar nera sprendimo iskart
+    if problem.goal_test(node.state): #Ar nėra sprendimo iškart
         return node
-    frontier = deque([node]) #Naujas pazistamu elementu sarasas su node parametru
-    explored = set() #Naujas istirtu elementu sarasas, tuscias
+    frontier = deque([node]) #Naujas pažistamų elementų sąrašas su node parametru
+    explored = set() #Naujas ištirtu elementų sąrašas, tuščias
     while frontier:
-        node = frontier.popleft() #Pajiemam pirma elementa
-        explored.add(tuple(map(tuple, node.state)))  #[[1, 2], [3, 4]] konvertuojame i ((1, 2), (3, 4)) ir idedam i istirtu elementu sarasa
-        for child in node.expand(problem): #Kol yra galimos vietos padeti varle
-            if tuple(map(tuple, child.state)) not in explored: #Jei elementas neistirtas
-                if problem.goal_test(child.state): #tai tikriname, ar nebaigiasi varles
+        node = frontier.popleft() #Pajiemam pirmą elementą
+        explored.add(tuple(map(tuple, node.state)))  #[[1, 2], [3, 4]] konvertuojame i ((1, 2), (3, 4)) ir idedam į istirtų elementų sąrašą
+        for child in node.expand(problem): #Kol yra galimos vietos padėti varlę
+            if tuple(map(tuple, child.state)) not in explored: #Jei elementas neištirtas
+                if problem.goal_test(child.state): #tai tikriname, ar nebaigiasi varlės
                     return child
-                frontier.append(child) #Jei ne, itraukiam elementa i pazistamu elementu saraso gala
+                frontier.append(child) #Jei ne, itraukiam elementą į pažistamu elementų sąrašo galą
                 explored.add(tuple(map(tuple, child.state)))  #
     return None
 
 class FrogsProblem:
-    def __init__(self, n, r): #Inicializacija kintamuju
-        self.n = n #pelkes dydis
-        self.r = r #varliu kiekis
-        self.initial = [[False] * n for _ in range(n)] #pelkes matrica
+    def __init__(self, n, r): #Inicializacija kintamujų
+        self.n = n #pelkės dydis
+        self.r = r #varlių kiekis
+        self.initial = [[False] * n for _ in range(n)] #pelkės matrica
 
-    def actions(self, state):#Paieska galimu varlems vietu
-        valid_actions = [] #tuscios aibes sukurimas
-        distance_map = [[0] * self.n for _ in range(self.n)]  #Distanciju zemelapis (matrica)
+    def actions(self, state):#Paieška galimų varlėms vietų
+        valid_actions = [] #tuščios aibės sukūrimas
+        distance_map = [[0] * self.n for _ in range(self.n)]  #Distancijų žemėlapis (matrica)
         for i in range(self.n):
             for j in range(self.n): #Dvimatis foras
                 if self.is_valid_position(state, i, j): #Jei i:j posicija laisva,
                     for x in range(self.n):
                         for y in range(self.n):  # Dvimatis foras
-                            if state[x][y]: #Jei x:y stovi varle
-                                distance_map[i][j] += pow(abs(x-i) + abs(y-j), 0.5) #Sumuojame prie zemelapio langelio distancijos ilgi. Kvadratinė šaknis reikalinga kad prioretizuoti langelius su atstumu nuo visu kitu varliu.
+                            if state[x][y]: #Jei x:y stovi varlė
+                                distance_map[i][j] += pow(abs(x-i) + abs(y-j), 0.5) #Sumuojame prie žemėlapio langelio distancijos ilgį. Kvadratinė šaknis reikalinga kad prioretizuoti langelius su atstumu nuo visų kitų varlių.
         ''' #for debug
         print("______________")
         for row in distance_map:
             print(" ".join(map(str, row)))
         #for debug '''
-        max_distance = max(max(row) for row in distance_map) #gauname maksimalia distancija
+        max_distance = max(max(row) for row in distance_map) #gauname maksimalią distanciją
         for i in range(self.n):
             for j in range(self.n):  # Dvimatis foras
-                if distance_map[i][j]==max_distance: #Jei zemelapyje keli maksimalios distancijos
-                    valid_actions.append((i, j)) #Visas prisijungiam prie galimu vietu aibes
+                if distance_map[i][j]==max_distance: #Jei žemėlapyje keli maksimalios distancijos
+                    valid_actions.append((i, j)) #Visas prisijungiam prie galimų vietų aibės
         return valid_actions
 
-    def result(self, state, action): #Varles idejimas
-        new_state = [row[:] for row in state] #Nauja pelkes versija ant senos pagrindo
-        new_state[action[0]][action[1]] = True #Pridejimas naujos varles
+    def result(self, state, action): #Varlės idėjimas
+        new_state = [row[:] for row in state] #Nauja pelkės versija ant senos pagrindo
+        new_state[action[0]][action[1]] = True #Padėjimas naujos varlės
         return new_state
 
-    def is_valid_position(self, state, i, j): #Ar yra i:j pozocoja laisva
-        if not (0 <= i < self.n and 0 <= j < self.n): #Jei kintamieji iseina uz lauko ribu
+    def is_valid_position(self, state, i, j): #Ar yra i:j pozicija laisva
+        if not (0 <= i < self.n and 0 <= j < self.n): #Jei kintamieji išeina už lauko ribų
             return False #Atmetama
-        if state[i][j]: #Ant pasirinktos vietos yra kita varle
+        if state[i][j]: #Ant pasirinktos vietos yra kita varlė
             return False #Atmetama
         return True
 
-    def goal_test(self, state):#Ar visi varles yra pelkeje
-        return sum(row.count(True) for row in state) == self.r #Skaiciuojama valriu suma ir liginama su paduotu pradzioje kiekiu
+    def goal_test(self, state):#Ar visi varlės yra pelkėje
+        return sum(row.count(True) for row in state) == self.r #Skaičiuojama valrių suma ir lyginama su paduotu pradžioje kiekių
 
 
 problem = FrogsProblem(5, 5) #Problemos sukurimas, kur r<=n*n
@@ -79,7 +79,7 @@ solution = breadth_first_graph_search(problem) #Problemos sprendimas
 
 if solution: #Jei sprendimas rastas
     print("Solution:")
-    for row in solution.state: #Isvedama matrica, kur O tai varles, o - tuscia vieta
+    for row in solution.state: #Išvedama matrica, kur O tai varlės, o - tuščia vieta
         print(" ".join("O" if cell else "-" for cell in row))
 else:
     print("Solution not found :(.") #Sprendimas nerastas
